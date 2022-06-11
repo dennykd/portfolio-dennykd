7a9d561046ddd27dc16a8ee7e2c364f1f69c0296
dennykd <kadekdenny61@gmail.com>
2022-06-11 15:08:11 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "20px",
+      },
+      colors: {
+        primary: "#14b8a6",
+        secondary: "#64748b",
+        dark: "#111827",
+      },
+      fontFamily: {
+        montserrat: ["montserrat", "sans-serif"],
+        lato: ["lato", "sans-serif"],
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
